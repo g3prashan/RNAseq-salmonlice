3ea3488ca3f0ce795cf2204d6ef2cc857af6a913
g3prashan <36694335+g3prashan@users.noreply.github.com>
2021-10-07 14:39:03 +0200

    Update and rename Workflow.txt to Workflow.sh

--- Workflow.sh
+++ Workflow.sh
@@ -16,12 +16,12 @@ wget ftp://ftp.ensemblgenomes.org/pub/metazoa/release-51/gff3/lepeophtheirus_sal
 hisat2-build -p 10 -f Lepeophtheirus_salmonis.LSalAtl2s.dna.toplevel.fa salmonlice_ind
 
 
-Abstract
-Total RNA was isolated from 16 adult male salmon lice of the drug susceptible strain IoA-00 and the multi-resistant strain IoA-02, using the Trizol protocol described in 2.7. Total isolated RNA was quantified by using Qubit 2.0 Fluorometer and Qubit RNA BR Assay Kit (Invitrogen, Thermo Fisher Scientific, UK). Quality of isolated total RNA was determined by UV spectrophotometry using a ND-1000 NanoDrop® (Thermo Scientific, UK) and Agilent Bioanalyzer system (Agilent Technologies, CA, USA). RNA integrity was assessed by electrophoresis using 100 ng of denaturized total RNA in a 1% agarose gel stained with ethidium bromide. Total mRNA of each individual was sequenced on an Illumina Novaseq 6000 platform at Novogene (Cambridge, United Kingdom).
+#Abstract
+#Total RNA was isolated from 16 adult male salmon lice of the drug susceptible strain IoA-00 and the multi-resistant strain IoA-02, using the Trizol protocol #described in 2.7. Total isolated RNA was quantified by using Qubit 2.0 Fluorometer and Qubit RNA BR Assay Kit (Invitrogen, Thermo Fisher Scientific, UK). Quality #of isolated total RNA was determined by UV spectrophotometry using a ND-1000 NanoDrop® (Thermo Scientific, UK) and Agilent Bioanalyzer system (Agilent #Technologies, CA, USA). RNA integrity was assessed by electrophoresis using 100 ng of denaturized total RNA in a 1% agarose gel stained with ethidium bromide. #Total mRNA of each individual was sequenced on an Illumina Novaseq 6000 platform at Novogene (Cambridge, United Kingdom).
 
-There were total 16 samples, we selected 8 to save space.
+#There were total 16 samples, we selected 8 to save space.
 
-Download the fastq files from ENA server
+#Download the fastq files from ENA server
 
 wget ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR496/ERR4968408/A8_1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR496/ERR4968408/A8_2.fastq.gz
@@ -51,7 +51,7 @@ wget ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR492/ERR4922192/A15_2.fastq.gz
 
 #Now use these files to make an alignment using HISAT2
 
-Use these code in a text file name ls_align_sam.txt 
+#Use these code in a text file name ls_align_sam.txt 
 
 hisat2 -p 24 --dta -x salmonlice_ind -1 /local/work/biocore/mol8008/mol8008_11/salmonlice/fastq_sra/A8_1.fastq.gz -2 /local/work/biocore/mol8008/mol8008_11/salmonlice/fastq_sra/A8_2.fastq.gz -S A8_sensitive.sam 2>summary8.txt
 
@@ -67,12 +67,12 @@ hisat2 -p 24 --dta -x salmonlice_ind -1 /local/work/biocore/mol8008/mol8008_11/s
 hisat2 -p 24 --dta -x salmonlice_ind -1 /local/work/biocore/mol8008/mol8008_11/salmonlice/fastq_sra/A14_1.fastq.gz -2 /local/work/biocore/mol8008/mol8008_11/salmonlice/fastq_sra/A14_2.fastq.gz -S A14_resistant.sam 2>summary14.txt
 
 
-make the txt file executable by 
+#make the txt file executable by 
 
 chmod 755 ls_align_sam.txt
 
 
-Run the alignment 
+#Run the alignment 
 
 ./ls_align_sam.txt
 
